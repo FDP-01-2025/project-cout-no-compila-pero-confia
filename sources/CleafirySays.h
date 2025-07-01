2989283f5dcf5d194e718d9a99af94ae465283f2
Miguelv293 <00143625@uca.edu.sv>
2025-06-30 18:51:31 -0600

    avance2(00143625)

--- sources/CleafirySays.h
+++ sources/CleafirySays.h
@@ -1,9 +1,13 @@
+#ifndef CLEAFIRYSAYS_H
+#define CLEAFIRYSAYS_H
+
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
 #include <thread>
 #include <chrono>
+#include <conio.h>
 
 #ifdef _WIN32
          #include <windows.h>
@@ -41,6 +45,52 @@ void showArrow(char arrow){
         case 'S': cout << "v" << endl; break;
         case 'D': cout << ">" << endl; break;
     }
+}
+
+int main(){
+    srand(static_cast<unsigned int>(time(0)));
+
+    vector<char> sequence;
+    vector<char> playerInput;
+    int level = 1;
+    bool playing = true;
+    int delay = 1000; // milliseconds between arrows
+    cout << "Welcome to 'Cleafiry Says'!" << endl;
+    cout << "Repeat the arrow sequence usign W (^), A (<), S(v), D(>)" <<  endl;
+    cout << "Press ENTER to start... " << endl;
+    cin.ignore();
+
+    while (playing){
+        // Add a new arrow to the sequence
+        sequence.push_back(getRandomArrow());
+
+        cout << "\nLevel " << level << "Watch the sequence... " << endl;
+
+        //Show the sequence
+        for (char arrow : sequence){
+            showArrow(arrow);
+            this_thread::sleep_for(chrono::milliseconds(delay));
+            clearScreen();
+        }
+
+        // Get player's input
+        playerInput.clear();
+        cout << "Now repeat the sequence" << endl;
+
+        for (size_t i = 0; i < sequence.size(); ++i){
+            char Input;
+            cout << "Arrow " << (i + 1) << ": ";
+            cin >> Input;
+            Input = toupper(Input);
+            playerInput.push_back(Input);
+        }
+
+        
+    }
+
+
+
+
 }
     
 
